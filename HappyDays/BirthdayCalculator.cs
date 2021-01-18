10826d6720d8b92d18a67a1c22460fa07e8f5708
Antonio Fernández Castillo <redmercury69@gmail.com>
2021-01-18 23:52:02 +0100

    Initial import

---
+++ HappyDays/BirthdayCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HappyDays
+{
+    public static class BirthdayCalculator
+    {
+        public static int CalculateDays(DateTime birthdate)
+        {
+            var nextBirthdate = new DateTime(DateTime.Now.Year, birthdate.Month, birthdate.Day);
+            var diff = nextBirthdate - DateTime.Now;
+
+            return diff.Days;
+        }
+    }
+}
